012e9d47a80f7c603426fcf8749537e7c6a2994a
Erika Stigleman <Erika.Stigleman@outlook.com>
2026-03-01 12:50:49 -0500

    start of contact page

--- security-assoc-inc/script.js
+++ security-assoc-inc/script.js
@@ -1,3 +1,20 @@
-//document.getElementById("contactBtn").addEventListener("click", function () {
-//alert("Contact form coming soon!");
-//});
+const serviceItems = document.querySelectorAll(".service-item");
+const servicesSection = document.querySelector(".services");
+
+serviceItems.forEach((item) => {
+	// Open one at a time
+	item.addEventListener("toggle", () => {
+		if (item.open) {
+			serviceItems.forEach((other) => {
+				if (other !== item) {
+					other.removeAttribute("open");
+				}
+			});
+		}
+	});
+});
+
+// Close all when mouse leaves services section
+servicesSection.addEventListener("mouseleave", () => {
+	serviceItems.forEach((item) => item.removeAttribute("open"));
+});
