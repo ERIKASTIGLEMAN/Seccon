e332d3e8113290de8b8c341aad8ee840d6d11d2d
Erika Stigleman <Erika.Stigleman@outlook.com>
2026-03-13 17:33:56 -0400

    modal close

--- security-assoc-inc/script.js
+++ security-assoc-inc/script.js
@@ -97,10 +97,8 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 
 		// Close modal when clicking outside the modal content
-		modal.addEventListener("click", (e) => {
-			if (e.target === modal) {
-				modal.style.display = "none";
-			}
+		modal.addEventListener("click", () => {
+			modal.style.display = "none";
 		});
 	}
 });
